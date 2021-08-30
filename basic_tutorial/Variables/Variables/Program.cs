829a58ac8b537c484d1627ee190592db932f0df0
Eduardo Santos Delgado <eduardosa@microsoft.com>
2021-08-30 06:33:38 -0700

    Collections (Array and lists)

--- basic_tutorial/Variables/Variables/Program.cs
+++ basic_tutorial/Variables/Variables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Variables
 {
@@ -40,6 +41,13 @@ namespace Variables
                     Console.WriteLine("I'm not zero");
                     break;
             }
+
+            int[] emptyArray = new int[5];
+            int[] arrayNumbers = new int[] { 1, 2, 3, 4, 5 };
+
+            List<int> intList = new List<int>() { 1, 2, 3};
+            intList.Add(5);
+            intList.Remove(1);
         }
 
         // Function returning nothing
