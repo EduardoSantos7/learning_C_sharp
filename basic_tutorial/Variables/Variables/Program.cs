d115fb4bfdc79597bd05c67dd7ecdf37e299b124
Eduardo Santos Delgado <eduardosa@microsoft.com>
2021-08-30 06:17:09 -0700

    variables and functions

---
+++ basic_tutorial/Variables/Variables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            int number1 = 7;
+            int number2 = 5;
+            Console.WriteLine($"The numbers in this console app are: {number1} and {number2}");
+
+            // Functions
+            Nothing();
+            bool bigger = isBiggerThan(5, 7);
+            Console.WriteLine($"{number1} > {number2}: " + bigger);
+        }
+
+        // Function returning nothing
+        static void Nothing()
+        {
+            Console.WriteLine("No returning data");
+        }
+
+        static bool isBiggerThan(int number, int target)
+        {
+            return number > target;
+        }
+    }
+}
