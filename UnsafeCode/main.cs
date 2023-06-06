f1465955faa04016540435ac6912c90de9d63a4a
Eduardo Santos Delgado <eduardosa@microsoft.com>
2023-06-05 22:56:49 -0700

    example of unsafe code 1

---
+++ UnsafeCode/main.cs
@@ -0,0 +1,11 @@
+using System;
+
+unsafe
+{
+    byte[] bytes = { 1, 2, 3 };
+    fixed (byte* pointerToFirst = bytes)
+    {
+        Console.WriteLine($"The address of the first array element: {(long)pointerToFirst:X}.");
+        Console.WriteLine($"The value of the first array element: {*pointerToFirst}.");
+    }
+}
